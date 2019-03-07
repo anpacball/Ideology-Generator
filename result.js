aafb2e9864ec54dd168de3bbd3d6873833546289
anpacball <47192596+anpacball@users.noreply.github.com>
2019-03-07 21:01:22 +0200

    Update result.js

--- result.js
+++ result.js
@@ -3,34 +3,49 @@ var ideologies = ["Queer", "Collapsitarian", "Techno-", "Traditionalist", "Feder
 "Gay", "Anarchist", "Post-Scarcity", "Individualist", "Market-", "Right-wing", "Left-wing", "Facistic", "HomoSexual", "Nigger",
 "Mafia", "Hedonist", "Egoistic", "Bolshevik", "Illegalist", "Accelerationist", "Darwinian", "Syndicalistic",
 "Neo-", "Post-", "Anti-", "Ultra-", "Judeo-", "Big-", "Woke", "Based", "Faggot-like", "Satanist",
-"Christian", "Islamist", "Jewish", "Ultra-gay", "Marxian", "Leninistic", "Dystopian Realist",
+"Christian", "Islamist", "Jewish", "Ultra-gay", "Marxian", "Leninistic", "DystopianRealist",
 "LeftCommunist", "Jucheist", "Althusserian", "Lacanian", "Zizekaian", "Foucaultian", "Dimmatarian", "Zionist", "Juden-", "LARPer",
-"Redundant", "Social-", "Democratic", "Anarcho-", "Leftism", "Workerist", "Chomskyan", "Cowboyist",
+"Redundant", "\"Open-Source\"-ist", "Social-", "Democratic", "Anarcho-", "Leftism", "Workerist", "Chomskyan", "Cowboyist",
 "Stirnerite-", "SocialDemocratic", "\"All tax is theft\"-ist", "Agorist", "Conservetive", "Liberal", "Reactionary", "Progressive",
 "Masochistic", "Anti-Police", "Trump-supporting", "Globalist", "Paleo-", "Soviet", "American-", "Scandinavian-",
-"Dictatorial", "Imperialist", "Authoritarian", "State-Enforced", "Paganist", "ClassicalLiberal", "Selfish",
+"Dictatorial","Clerical", "Imperialist", "Authoritarian", "State-Enforced", "Paganist", "ClassicalLiberal", "Selfish",
 "Creatonist", "Egalitarian", "Constitutionalist", "Counter-Economical", "Government", "Ethno-", "Stateist",
 "Post-left", "Bureaucratic", "Platonism", "Pacifist", "Militant", "Revolutionary", "Insurrectionist", "Third-Worldist",
-"Gandhian", "Esoteric", "Absolute-", "Orthodox"];
+"Gandhian", "Esoteric", "Absolute-", "Orthodox", "Cyber-", "Situationist", "Öcalanist-", "Crypto-", "FreeMarket",
+"Reinfeldtist-", "Francoist", "Brezjnevist", "Unorthodoxical-", "Collectivist", "Kurdish", "Arab", "Isreali", "French",
+"Equatorial", "Lobbyist-", "Veganism", "Vegetarianism", "Hegalian", "Engelian", "Merkelist", "Pinochetian-"];
 
 
 var endings = ["Strasserist", "Maoist", "Stalinist", "Libertarian", "Communist", "Socialist", "Marxist", "Minarchist",
 "SocialDemocrat", "Mutualist", "Objectivist", "Syndicalist", "Capitalist", "ClassicalLiberal", "Leninist", "Hoaxaist",
 "PolPotist", "Titoist", "Facist", "Nazi", "Moderate", "Egoist", "Voluntaryism", "Black Liberationist", "Neo-Abolitionist",
-"Putinist", "Feralist", "Laissez-Faire Capitalist", "Royalist", "Monarchist", "Posadist"];
+"Putinist", "Feralist", "Laissez-Faire Capitalist", "Royalist", "Monarchist", "Posadist", "Trotskyism", "Erdoganism",
+"Tumblrist", "Populist", "Thatcherite"];
 
-var state = ["Extreme", "Moderate", "Agnostic", "Mild", "Dystopian", "Ultra Extreme", "Radical"];
+var state = ["Extreme", "Moderate", "Agnostic", "Mild", "Dystopian", "Ultra Extreme", "Radical", "Insanly Radical"];
 
 console.log("With " + ((ideologies.length + 1) + (endings.length + 1) + (state.length + 1)) + " Ideologies!");
 
 function getx() {
-  return Math.floor(Math.random() * 100) + 1;
+  return Math.floor(Math.random() * (ideologies.length - 1));
 }
 function gety(xx = ideologies) {
   return Math.floor(Math.random() * xx.length);
 }
+var used = [];
+var word_counter = 0;
+function check(x) {
+  for (var i = 0; i < used.length; i++) {
+    if (used[i] == x) {
+      console.log("[Double] used: " + used[i] + " x: " + x + " Word: " + ideologies[used[i]] + " Count:" + word_counter);
+      return true;
+    }
+  }
+  return false;
+}
 
 function result() {
+  console.log("---------------------------------------");
   var res = document.getElementById("res");
   var all_html = "";
 
@@ -39,16 +54,16 @@ function result() {
 
   var x = getx();
   var y = gety();
-  var used = [];
+
   for (var i = 0; i < x; i++) {
-    var already_used = false;
+    var already_used = check(i);
+    word_counter += 1;
     var sep = " ";
-    // TODO: Add used and make it check if ideology is already in string.
 
     if (!already_used) {
       if (ideologies[y].slice(-1) == "-") { sep = ""; }
       all_html = all_html + ideologies[y] + sep;
-      used.push(i);
+      used.push(y);
       x = getx();
       y = gety();
     }
@@ -56,11 +71,18 @@ function result() {
 
   all_html = all_html + endings[gety(endings)];
 
+
+  if (Math.floor(Math.random() * 100) == 3) {
+    var board = ["/pol/", "/g/", "/lit/", "/b/", "/v/", "/h/", "/k/"];
+    all_html += " From " + board[Math.floor(Math.random() * board.length)];
+  }
+
   if (Math.floor(Math.random() * 20) == 3) {
     var countries = ["Irish", "Chinese", "Indian", "Swedish", "Finnish", "German", "Russian", "Austrailian", "Italian"];
     var country = countries[Math.floor(Math.random() * countries.length)];
     all_html = all_html + " With " + country + " Characteristics!"
   }
-
+  used = [];
+  word_counter = 0;
   res.innerHTML = "<h3>" + all_html + "</h3>";
 }
