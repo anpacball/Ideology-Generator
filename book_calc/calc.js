7c6d47aa1ccca3475dfc915ee7311b31f79ecc93
anpacball <47192596+anpacball@users.noreply.github.com>
2019-05-12 21:41:40 +0300

    Update calc.js

--- book_calc/calc.js
+++ book_calc/calc.js
@@ -1,3 +1,7 @@
+function procent(value) {
+  return ~~((value) * 100) + "%";
+}
+
 function calc() {
   var bokens_langd = document.getElementById("sidor").value;
   var sidor_pd = document.getElementById("tid").value;// Sidor per dag.
@@ -15,19 +19,19 @@ function calc() {
   output += speed + " dagar att läsa boken vid denna hastighet{DOT}<br>"
   var gardet = speed / lantid;
   if ((gardet / omlan) >= finetune) {
-    output += "[!] Du kommer inte hinna, du har " + gardet + " omlån{DOT}";
+    output += "[!] Du kommer inte hinna, det kommer kräva " + gardet + " omlån{DOT}";
     det_finns_tid = false;
   }
   else {
-    output += "Detta betyder att du kommer hinna med " + gardet + " omlån{DOT}";
+    output += "Detta betyder att du kommer hinna använda " + gardet + " omlån{DOT}";
     det_finns_tid = true;
   }
   output += "<br>";
 
   var table = '<table class="table"><thead class="thead-light"><tr><th scope="col">#</th>';
   table += '<th scope="col">' + 'Dagar' + '</th>';
-  table += '<th scope="col">' + 'Lån' + '</th>';
-  table += '<th scope="col">' + '"Fine Tune"' + '</th></tr></thead><tbody>';
+  table += '<th scope="col">' + 'Lån (%)' + '</th>';
+  table += '<th scope="col">' + 'Med omlån (%)' + '</th></tr></thead><tbody>';
 
   var og = sidor_pd;
   sidor_pd -= 2;
@@ -35,6 +39,7 @@ function calc() {
   while (x < 5) {
 
     var finer = (bokens_langd / sidor_pd / lantid / omlan);
+    var pct = procent(finer);
     if (sidor_pd == og) {
       if (det_finns_tid) {
         table += '<tr class="table-primary"><th scope="row">' + sidor_pd + '</th><td>';
@@ -50,14 +55,15 @@ function calc() {
         table += '<tr class="table-success"><th scope="row">' + sidor_pd + '</th><td>';
       }
     }
-    table += (bokens_langd / sidor_pd) + '</td><td>';
-    table += (bokens_langd / sidor_pd / lantid) + '</td>';
-    if (finer >= finetune) {
-      table += '<td>' + finer + ' >= ' + finetune + '</td></tr>';
+    var sidor_ = bokens_langd / sidor_pd;
+    if (Number.isInteger(sidor_)) {
+      table += sidor_ + '</td><td>';
     }
     else {
-      table += '<td>' + finer + ' < ' + finetune + '</td></tr>';
+      table += sidor_.toFixed(3) + '</td><td>';
     }
+    table += procent(bokens_langd / sidor_pd / lantid) + '</td><td>';
+    table += pct + '</td></tr>';
     sidor_pd++;
     x++;
   }
