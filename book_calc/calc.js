919d7f1bae5b2a1e13ee6884413c63c8c48ddbff
anpacball <47192596+anpacball@users.noreply.github.com>
2019-05-12 19:00:25 +0300

    Update calc.js

--- book_calc/calc.js
+++ book_calc/calc.js
@@ -1,20 +1,72 @@
 function calc() {
   var bokens_langd = document.getElementById("sidor").value;
   var sidor_pd = document.getElementById("tid").value;// Sidor per dag.
+  if (sidor_pd <= 1) {
+    alert("För dum fråga. Ha ett högre \"Sido Per Dag\"-värde än 1.");
+    return;
+  }
   const lantid = 30;
   const omlan = 6;
-  const finetune = 0.9;
+  const finetune = 0.85;
+  var det_finns_tid = false;
   var output = "Det kommer att ta dig ";
 
   var speed = bokens_langd / sidor_pd;
-  output += speed + " dagar att läsa boken vid denna hastighet.<br>"
+  output += speed + " dagar att läsa boken vid denna hastighet{DOT}<br>"
   var gardet = speed / lantid;
   if ((gardet / omlan) >= finetune) {
-    output += "[!] Du kommer inte hinna, du har " + gardet + " omlån.";
+    output += "[!] Du kommer inte hinna, du har " + gardet + " omlån{DOT}";
+    det_finns_tid = false;
   }
   else {
-    output += "Detta betyder att du kommer hinna med " + gardet + " omlån.";
+    output += "Detta betyder att du kommer hinna med " + gardet + " omlån{DOT}";
+    det_finns_tid = true;
   }
+  output += "<br>";
+
+  var table = '<table class="table"><thead class="thead-light"><tr><th scope="col">#</th>';
+  table += '<th scope="col">' + 'Dagar' + '</th>';
+  table += '<th scope="col">' + 'Lån' + '</th>';
+  table += '<th scope="col">' + '"Fine Tune"' + '</th></tr></thead><tbody>';
+
+  var og = sidor_pd;
+  sidor_pd -= 2;
+  var x = 0;
+  while (x < 5) {
+
+    var finer = (bokens_langd / sidor_pd / lantid / omlan);
+    if (sidor_pd == og) {
+      if (det_finns_tid) {
+        table += '<tr class="table-primary"><th scope="row">' + sidor_pd + '</th><td>';
+      } else {
+        table += '<tr class="table-warning"><th scope="row">' + sidor_pd + '</th><td>';
+      }
+    }
+    else {
+      if (finer >= finetune) {
+        table += '<tr class="table-danger"><th scope="row">' + sidor_pd + '</th><td>';
+      }
+      else {
+        table += '<tr class="table-success"><th scope="row">' + sidor_pd + '</th><td>';
+      }
+    }
+    table += (bokens_langd / sidor_pd) + '</td><td>';
+    table += (bokens_langd / sidor_pd / lantid) + '</td>';
+    if (finer >= finetune) {
+      table += '<td>' + finer + ' >= ' + finetune + '</td></tr>';
+    }
+    else {
+      table += '<td>' + finer + ' < ' + finetune + '</td></tr>';
+    }
+    sidor_pd++;
+    x++;
+  }
+  table += '</tbody></table>';
+
+  output +=  "<br>" + table;
+  output = output.replace(/\./g, ",");
+  output = output.replace(/{DOT}/g, ".");
+
 
   document.getElementById("result_box").innerHTML = output;
 }
