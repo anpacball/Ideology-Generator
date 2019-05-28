15bcb02889593e0de9fc18f98600b05b4e8b420d
anpacball <47192596+anpacball@users.noreply.github.com>
2019-05-28 11:41:22 +0500

    Update calc.js

--- book_calc/calc.js
+++ book_calc/calc.js
@@ -2,68 +2,93 @@ function procent(value) {
   return ~~((value) * 100) + "%";
 }
 
+document.onkeydown = function(event) {
+  switch (event.keyCode) {
+    case 38:
+      document.getElementById("tid").value--;
+      calc();
+      break;
+    case 40:
+      document.getElementById("tid").value++;
+      calc();
+      break;
+    case 13:
+      calc();
+      break;
+    default:
+      //alert(event.keyCode);
+  }
+};
+
 function calc() {
   var bokens_langd = document.getElementById("sidor").value;
   var sidor_pd = document.getElementById("tid").value;// Sidor per dag.
   if (sidor_pd <= 1) {
-    alert("För dum fråga. Ha ett högre \"Sido Per Dag\"-värde än 1.");
+    document.getElementById("tid").value++;
+    if (document.getElementById("tid").value <= 1) {
+      var processor = setInterval(function()
+      {
+        if (document.getElementById("tid").value <= 1) {
+          document.getElementById("tid").value++;
+        }
+        else {
+          clearInterval(processor);
+          calc();
+          return;
+        }
+      }, 100);
+    }
     return;
   }
   const lantid = 30;
   const omlan = 6;
   const finetune = 0.85;
-  var det_finns_tid = false;
   var output = "Det kommer att ta dig ";
 
   var speed = bokens_langd / sidor_pd;
   output += speed + " dagar att läsa boken vid denna hastighet{DOT}<br>"
   var gardet = speed / lantid;
-  if ((gardet / omlan) >= finetune) {
-    output += "[!] Du kommer inte hinna, det kommer kräva " + gardet + " omlån{DOT}";
-    det_finns_tid = false;
-  }
-  else {
-    output += "Detta betyder att du kommer hinna använda " + gardet + " omlån{DOT}";
-    det_finns_tid = true;
-  }
+  if ((gardet / omlan) >= finetune) { output += "[!] Du kommer inte hinna, det kommer kräva " + gardet + " lån{DOT}"; }
+  else { output += "Detta betyder att du kommer hinna använda " + gardet + " lån{DOT}"; }
   output += "<br>";
 
   var table = '<table class="table"><thead class="thead-light"><tr><th scope="col">#</th>';
   table += '<th scope="col">' + 'Dagar' + '</th>';
+  table += '<th scope="col">' + 'Timmar' + '</th>';
   table += '<th scope="col">' + 'Lån (%)' + '</th>';
   table += '<th scope="col">' + 'Med omlån (%)' + '</th></tr></thead><tbody>';
 
   var og = sidor_pd;
   sidor_pd -= 2;
   var x = 0;
+  speed = -1;
   while (x < 5) {
 
     var finer = (bokens_langd / sidor_pd / lantid / omlan);
-    var pct = procent(finer);
-    if (sidor_pd == og) {
-      if (det_finns_tid) {
-        table += '<tr class="table-primary"><th scope="row">' + sidor_pd + '</th><td>';
-      } else {
-        table += '<tr class="table-warning"><th scope="row">' + sidor_pd + '</th><td>';
-      }
-    }
-    else {
+    var speed = bokens_langd / sidor_pd;
+    var dclass = "";
+
+    if (speed >= 29) {
       if (finer >= finetune) {
-        table += '<tr class="table-danger"><th scope="row">' + sidor_pd + '</th><td>';
+        if (sidor_pd == og) { dclass += 'class="table-primary" id="txt_danger"'; }
+        else { dclass += 'class="table-danger"'; }
       }
       else {
-        table += '<tr class="table-success"><th scope="row">' + sidor_pd + '</th><td>';
+        if (sidor_pd == og) { dclass += 'class="table-primary" id="txt_warning"'; }
+        else { dclass += 'class="table-warning"'; }
       }
     }
-    var sidor_ = bokens_langd / sidor_pd;
-    if (Number.isInteger(sidor_)) {
-      table += sidor_ + '</td><td>';
-    }
     else {
-      table += sidor_.toFixed(3) + '</td><td>';
+      if (sidor_pd == og) { dclass += 'class="table-primary" id="txt_soft"'; }
+      else { dclass += 'class="table-success"'; }
     }
+
+    table += '<tr ' + dclass + '><th scope="row">' + sidor_pd + '</th><td>';
+
+    table += Math.floor(speed) + '</td><td>';
+    table += Math.round((speed % 1) * 24) + '</td><td>';
     table += procent(bokens_langd / sidor_pd / lantid) + '</td><td>';
-    table += pct + '</td></tr>';
+    table += procent(finer) + '</td></tr>';
     sidor_pd++;
     x++;
   }
